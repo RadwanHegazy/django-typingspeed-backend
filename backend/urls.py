b328c4ed87c1f62db6b8649c408fd663956dfdf3
RadwanHegazy <radwangaber22@gmail.com>
2024-04-22 15:59:06 +0200

    main configuration

--- backend/urls.py
+++ backend/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('user/',include('users.apis.urls')),
+    path('battle/',include('battle.apis.urls')),
 ] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
 
